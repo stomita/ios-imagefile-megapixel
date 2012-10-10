12b285140798a72a85fe428bcd045ea5d874f977
Shinichi Tomita <shinichi.tomita@gmail.com>
2012-10-10 23:04:40 +0900

    add license notes

--- src/megapix-image.js
+++ src/megapix-image.js
@@ -4,6 +4,9 @@
  * Fixes iOS6 Safari's image file rendering issue for large size image (over mega-pixel),
  * which causes unexpected subsampling when drawing it in canvas.
  * By using this library, you can safely render the image with proper stretching.
+ *
+ * Copyright (c) 2012 Shinichi Tomita <shinichi.tomita@gmail.com>
+ * Released under the MIT license
  */
 (function() {
 
